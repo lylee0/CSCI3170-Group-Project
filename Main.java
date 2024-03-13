62da49f279c98eb1a47ab5b80ceb4222c72a7992
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-13 23:22:58 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
     class CustomerInterface {
         void print_menu() {
             System.out.println("<This is the customer interface.>");
-            System.out.println("-------------------------------");
+            System.out.println("---------------------------------");
             System.out.println("1. Book Search.");
             System.out.println("2. Order Creation.");
             System.out.println("3. Order Altering.");
