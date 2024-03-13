04305cb514c57dd464e02d8ba841640766808975
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-13 22:44:15 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,14 @@ import java.util.*;
 public class Main {
     class SystemInterface {
         void print_menu() {
-
+            System.out.println("<This is the system interface.>");
+            System.out.println("-------------------------------");
+            System.out.println("1. Create Table.");
+            System.out.println("2. Delete Table.");
+            System.out.println("3. Insert Data.");
+            System.out.println("4. Set System Date.");
+            System.out.println("5. Back to main menu.");
+            System.out.println("Please enter your choice??..");
         }
 
         void loop() {
