07b6681f90fab68a64d6361a24f32c2b2775c282
Gordon <1155159410@link.cuhk.edu.hk>
2024-03-16 15:01:45 +0800

    Update Main.java, SystemInterface.java
    - Split 'SystemInterface' out.

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-import java.io.*;
 import java.sql.*;
 import java.time.*;
 import java.util.*;
 
 public class Main {
+    // Instance objects that should be inherited by every subclass
     Connection conn;
-    int system_date = 0;
+    IntWrapper system_date = new IntWrapper(0);
 
     class ExecuteQuery {
         Statement stmt;
@@ -30,211 +30,6 @@ public class Main {
                 stmt.close();
             } catch (Exception e) {
                 System.err.println("Failed to close resources: " + e.getMessage());
-            }
-        }
-    }
-
-    class SystemInterface {
-        static String[] table_names = {"book", "customer", "orders", "ordering", "book_author"};
-
-        /* Create all tables regardless of errors. */
-        void create_table() {
-            Map<String, String> table_definitions = new LinkedHashMap<>();
-            table_definitions.put("book", """
-                    CREATE TABLE book
-                    (
-                        isbn         INTEGER,
-                        title        VARCHAR(100),
-                        unit_price   INTEGER,
-                        no_of_copies INTEGER,
-                        PRIMARY KEY (isbn)
-                    )
-                    """);
-            table_definitions.put("customer", """
-                    CREATE TABLE customer
-                    (
-                        customer_id      VARCHAR(10),
-                        name             VARCHAR(50),
-                        shipping_address VARCHAR(200),
-                        credit_card_no   INTEGER,
-                        PRIMARY KEY (customer_id)
-                    )
-                    """);
-            table_definitions.put("orders", """
-                    CREATE TABLE orders
-                    (
-                        order_id        INTEGER,
-                        o_date          INTEGER,
-                        shipping_status CHAR,
-                        charge          INTEGER,
-                        customer_id     VARCHAR(10),
-                        PRIMARY KEY (order_id),
-                        FOREIGN KEY (customer_id) REFERENCES customer
-                    )
-                    """);
-            table_definitions.put("ordering", """
-                    CREATE TABLE ordering
-                    (
-                        order_id INTEGER,
-                        isbn     INTEGER,
-                        quantity INTEGER,
-                        PRIMARY KEY (order_id, isbn),
-                        FOREIGN KEY (order_id) REFERENCES orders,
-                        FOREIGN KEY (isbn) REFERENCES book
-                    )
-                    """);
-            table_definitions.put("book_author", """
-                    CREATE TABLE book_author
-                    (
-                        isbn        INTEGER,
-                        author_name VARCHAR(50),
-                        PRIMARY KEY (isbn, author_name),
-                        FOREIGN KEY (isbn) REFERENCES book
-                    )
-                    """);
-
-            table_definitions.forEach((table_name, table_definition) -> {
-                if (execute_update(table_definition)) System.out.printf("Table '%s' has been created.\n", table_name);
-                else System.out.printf("Failed to create table '%s'.\n", table_name);
-            });
-        }
-
-        /* Delete all tables regardless of errors. */
-        void delete_table() {
-            for (String table_name : table_names) {
-                String sql_statement = String.format("DROP TABLE %s CASCADE CONSTRAINTS", table_name);
-                if (execute_update(sql_statement)) System.out.printf("Table '%s' has been deleted.\n", table_name);
-                else System.out.printf("Failed to delete table '%s'.\n", table_name);
-            }
-        }
-
-        void insert_data() {
-            List<File> data_files = new ArrayList<>();
-
-            System.out.println("Please enter the folder path:");
-
-            // Check if the directory is appropriate
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            try {
-                String directory = input;
-                for (String table_name : table_names) {
-                    String filename = table_name + ".txt";
-                    data_files.add(new File(directory, filename));
-
-                    // Check whether the latest added file exists
-                    if (!data_files.get(data_files.size() - 1).exists())
-                        throw new Exception(String.format("'%s' does not exist in the directory.", filename));
-                }
-            } catch (Exception e) {
-                System.err.println("Failed to get data: " + e.getMessage());
-                return;
-            }
-
-            // Define the format specifiers for the tuple of tables
-            String[] tuple_formats = {"(%s, '%s', %s, %s)",  // book
-                    "('%s', '%s', '%s', %s)",  // customer
-                    "(%s, %s, '%s', %s, '%s')",  // orders
-                    "(%s, %s, %s)",  // ordering
-                    "(%s, '%s')",  // book_author
-            };
-
-            // Start to insert data
-            System.out.println("Processing...");
-            try {
-                for (int i = 0; i < table_names.length; i++) {
-                    BufferedReader reader = new BufferedReader(new FileReader(data_files.get(i)));
-
-                    // Read line by line until EOF
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        String[] parts = line.split("\\|");
-
-                        // Format input data according to the specific table
-                        if (i == 0) {
-                            // book (isbn, title, unit_price, no_of_copies)
-                            parts[0] = parts[0].replace("-", "");
-                        } else if (i == 1) {
-                            // customer (customer_id, name, shipping_address, credit_card_no)
-                            parts[3] = parts[3].replace("-", "");
-                        } else if (i == 2) {
-                            // orders (order_id, o_date, shipping_status, charge, customer_id)
-                            parts[1] = Integer.toString(date_str_to_int(parts[1]));
-                        } else if (i == 3) {
-                            // ordering (order_id, isbn, quantity)
-                            parts[1] = parts[1].replace("-", "");
-                        } else if (i == 4) {
-                            // book_author (isbn, author_name)
-                            parts[0] = parts[0].replace("-", "");
-                        }
-
-                        // Construct the SQL update statement
-                        String tuple_str = String.format(tuple_formats[i], (Object[]) parts);
-                        String sql_statement = String.format("INSERT INTO %s VALUES ", table_names[i]) + tuple_str;
-
-                        // Execute the statement
-                        String stmt_msg = String.format("%s %s", table_names[i], tuple_str);
-                        if (execute_update(sql_statement)) System.out.println("Inserted: " + stmt_msg);
-                        else throw new Exception(stmt_msg);
-                    }
-                }
-                System.out.println("All data loaded!");
-            } catch (Exception e) {
-                System.err.println("Failed to insert data: " + e.getMessage());
-            }
-        }
-
-        void set_system_date() {
-            System.out.print("Please input the date (YYYYMMDD): ");
-
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            while (!verify_date_str(input)) {
-                System.out.print("Invalid input. Please try again: ");
-                input = scanner.nextLine();
-            }
-
-            // Update system date
-            system_date = date_str_to_int(input);
-
-            // Get latest date in orders
-            try {
-                ExecuteQuery query = new ExecuteQuery("SELECT MAX(o_date) FROM orders");
-                query.rs.next();  // Move the cursor to the 1st row of the result set
-                int latest_order_date = query.rs.getInt(1);  // 0 if the table is empty
-                query.close();
-
-                // Print the date only if query is successful
-                System.out.println("Latest date in orders: " + date_int_to_str(latest_order_date));
-            } catch (Exception e) {
-                System.err.println("Failed to get latest order date: " + e.getMessage());
-            }
-
-            // Print current system date
-            System.out.println("Today is " + date_int_to_str(system_date));
-        }
-
-        void print_menu() {
-            System.out.println("\n<This is the system interface>");
-            System.out.println("-------------------------------");
-            System.out.println("1. Create table");
-            System.out.println("2. Delete table");
-            System.out.println("3. Insert data");
-            System.out.println("4. Set system date");
-            System.out.println("5. Back to main menu");
-            System.out.print("\nPlease enter your choice??..");
-        }
-
-        void loop() {
-            while (true) {
-                print_menu();
-                int choice = get_user_choice(5);
-
-                if (choice == 1) create_table();
-                else if (choice == 2) delete_table();
-                else if (choice == 3) insert_data();
-                else if (choice == 4) set_system_date();
-                else break;
             }
         }
     }
@@ -379,7 +174,7 @@ public class Main {
     }
 
     void show_system_date() {
-        System.out.println("\nThe system date is now: " + date_int_to_str(system_date));
+        System.out.println("\nThe system date is now: " + date_int_to_str(system_date.value));
     }
 
     void print_menu() {
@@ -403,7 +198,7 @@ public class Main {
             print_menu();
             int choice = get_user_choice(5);
 
-            if (choice == 1) new SystemInterface().loop();
+            if (choice == 1) new SystemInterface(this).loop();
             else if (choice == 2) new CustomerInterface().loop();
             else if (choice == 3) new BookstoreInterface().loop();
             else if (choice == 4) show_system_date();
--- SystemInterface.java
+++ SystemInterface.java
@@ -1,68 +1,215 @@
-//package Project.codes;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
 
-public class SystemInterface{
-    private Scanner scanner;
+class SystemInterface extends Main {
+    static String[] table_names = {"book", "customer", "orders", "ordering", "book_author"};
 
-    public void start(){
-        printOptions();
-        scanner = new Scanner(System.in);
-        Integer input = getInput(scanner);
-        run(input, scanner);
+    SystemInterface(Main parent_instance) {
+        // inherit instance objects from the parent instance
+        conn = parent_instance.conn;
+        system_date = parent_instance.system_date;
     }
 
-    public void printOptions(){
-        System.out.println("<This is the system interface.>");
+    /* Create all tables regardless of errors. */
+    void create_table() {
+        Map<String, String> table_definitions = new LinkedHashMap<>();
+        table_definitions.put("book", """
+                CREATE TABLE book
+                (
+                    isbn         INTEGER,
+                    title        VARCHAR(100),
+                    unit_price   INTEGER,
+                    no_of_copies INTEGER,
+                    PRIMARY KEY (isbn)
+                )
+                """);
+        table_definitions.put("customer", """
+                CREATE TABLE customer
+                (
+                    customer_id      VARCHAR(10),
+                    name             VARCHAR(50),
+                    shipping_address VARCHAR(200),
+                    credit_card_no   INTEGER,
+                    PRIMARY KEY (customer_id)
+                )
+                """);
+        table_definitions.put("orders", """
+                CREATE TABLE orders
+                (
+                    order_id        INTEGER,
+                    o_date          INTEGER,
+                    shipping_status CHAR,
+                    charge          INTEGER,
+                    customer_id     VARCHAR(10),
+                    PRIMARY KEY (order_id),
+                    FOREIGN KEY (customer_id) REFERENCES customer
+                )
+                """);
+        table_definitions.put("ordering", """
+                CREATE TABLE ordering
+                (
+                    order_id INTEGER,
+                    isbn     INTEGER,
+                    quantity INTEGER,
+                    PRIMARY KEY (order_id, isbn),
+                    FOREIGN KEY (order_id) REFERENCES orders,
+                    FOREIGN KEY (isbn) REFERENCES book
+                )
+                """);
+        table_definitions.put("book_author", """
+                CREATE TABLE book_author
+                (
+                    isbn        INTEGER,
+                    author_name VARCHAR(50),
+                    PRIMARY KEY (isbn, author_name),
+                    FOREIGN KEY (isbn) REFERENCES book
+                )
+                """);
+
+        table_definitions.forEach((table_name, table_definition) -> {
+            if (execute_update(table_definition)) System.out.printf("Table '%s' has been created.\n", table_name);
+            else System.out.printf("Failed to create table '%s'.\n", table_name);
+        });
+    }
+
+    /* Delete all tables regardless of errors. */
+    void delete_table() {
+        for (String table_name : table_names) {
+            String sql_statement = String.format("DROP TABLE %s CASCADE CONSTRAINTS", table_name);
+            if (execute_update(sql_statement)) System.out.printf("Table '%s' has been deleted.\n", table_name);
+            else System.out.printf("Failed to delete table '%s'.\n", table_name);
+        }
+    }
+
+    void insert_data() {
+        List<File> data_files = new ArrayList<>();
+
+        System.out.println("Please enter the folder path:");
+
+        // Check if the directory is appropriate
+        Scanner scanner = new Scanner(System.in);
+        String folder_path = scanner.nextLine();
+        try {
+            for (String table_name : table_names) {
+                String filename = table_name + ".txt";
+                data_files.add(new File(folder_path, filename));
+
+                // Check whether the latest added file exists
+                if (!data_files.get(data_files.size() - 1).exists())
+                    throw new Exception(String.format("'%s' does not exist in the directory.", filename));
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to get data: " + e.getMessage());
+            return;
+        }
+
+        // Define the format specifiers for the tuple of tables
+        String[] tuple_formats = {"(%s, '%s', %s, %s)",  // book
+                "('%s', '%s', '%s', %s)",  // customer
+                "(%s, %s, '%s', %s, '%s')",  // orders
+                "(%s, %s, %s)",  // ordering
+                "(%s, '%s')",  // book_author
+        };
+
+        // Start to insert data
+        System.out.println("Processing...");
+        try {
+            for (int i = 0; i < table_names.length; i++) {
+                BufferedReader reader = new BufferedReader(new FileReader(data_files.get(i)));
+
+                // Read line by line until EOF
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split("\\|");
+
+                    // Format input data according to the specific table
+                    if (i == 0) {
+                        // book (isbn, title, unit_price, no_of_copies)
+                        parts[0] = parts[0].replace("-", "");
+                    } else if (i == 1) {
+                        // customer (customer_id, name, shipping_address, credit_card_no)
+                        parts[3] = parts[3].replace("-", "");
+                    } else if (i == 2) {
+                        // orders (order_id, o_date, shipping_status, charge, customer_id)
+                        parts[1] = Integer.toString(date_str_to_int(parts[1]));
+                    } else if (i == 3) {
+                        // ordering (order_id, isbn, quantity)
+                        parts[1] = parts[1].replace("-", "");
+                    } else if (i == 4) {
+                        // book_author (isbn, author_name)
+                        parts[0] = parts[0].replace("-", "");
+                    }
+
+                    // Construct the SQL update statement
+                    String tuple_str = String.format(tuple_formats[i], (Object[]) parts);
+                    String sql_statement = String.format("INSERT INTO %s VALUES ", table_names[i]) + tuple_str;
+
+                    // Execute the statement
+                    String stmt_msg = String.format("%s %s", table_names[i], tuple_str);
+                    if (execute_update(sql_statement)) System.out.println("Inserted: " + stmt_msg);
+                    else throw new Exception(stmt_msg);
+                }
+            }
+            System.out.println("All data loaded!");
+        } catch (Exception e) {
+            System.err.println("Failed to insert data: " + e.getMessage());
+        }
+    }
+
+    void set_system_date() {
+        System.out.print("Please input the date (YYYYMMDD): ");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!verify_date_str(input)) {
+            System.out.print("Invalid input. Please try again: ");
+            input = scanner.nextLine();
+        }
+
+        // Update system date
+        system_date.value = date_str_to_int(input);
+
+        // Get latest date in orders
+        try {
+            Main.ExecuteQuery query = new Main.ExecuteQuery("SELECT MAX(o_date) FROM orders");
+            query.rs.next();  // Move the cursor to the 1st row of the result set
+            int latest_order_date = query.rs.getInt(1);  // 0 if the table is empty
+            query.close();
+
+            // Print the date only if query is successful
+            System.out.println("Latest date in orders: " + date_int_to_str(latest_order_date));
+        } catch (Exception e) {
+            System.err.println("Failed to get latest order date: " + e.getMessage());
+        }
+
+        // Print current system date
+        System.out.println("Today is " + date_int_to_str(system_date.value));
+    }
+
+    void print_menu() {
+        System.out.println("\n<This is the system interface>");
         System.out.println("-------------------------------");
-        System.out.println("1. Create Table.");
-        System.out.println("2. Delete Table.");
-        System.out.println("3. Insert Data.");
-        System.out.println("4. Set System Date.");
-        System.out.println("5. Back to main menu.");
+        System.out.println("1. Create table");
+        System.out.println("2. Delete table");
+        System.out.println("3. Insert data");
+        System.out.println("4. Set system date");
+        System.out.println("5. Back to main menu");
+        System.out.print("\nPlease enter your choice??..");
     }
 
-    public getInput(Scanner scanner){
-        System.out.println("\nWhat is your choice??..");
-        Integer input = Integer.parseInt(scanner.next());
-        return input;
-    }
+    /* Must set 'public' since this method is 'public' in the superclass */
+    public void loop() {
+        while (true) {
+            print_menu();
+            int choice = get_user_choice(5);
 
-    public void run(Integer input, Scanner scanner){
-        if (input == 1) {
-            createTable();
-        } else if (input == 2) {
-            deleteTable();
-        } else if (input == 3) {
-            insertData();
-        } else if (input == 4) {
-            setDate();
-        } else if (input == 5){
-            Main.main(new String[0]);  // change to the name of main function
+            if (choice == 1) create_table();
+            else if (choice == 2) delete_table();
+            else if (choice == 3) insert_data();
+            else if (choice == 4) set_system_date();
+            else break;
         }
-        else {
-            System.out.println("Invalid input, please try again.");
-            input = getInput(scanner);
-            run(input, scanner);
-        }
-    }
-
-    private void createTable(){
-        // to be done
-        start();
-    }
-
-    private void deleteTable(){
-        // to be done
-        start();
-    }
-
-    private void insertData(){
-        // to be done
-        start();
-    }
-
-    private void setDate(){
-        // to be done
-        start();
     }
 }
